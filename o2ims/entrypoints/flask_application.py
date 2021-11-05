fa751ff80f781c98ae67f85407d263560ff340de
Zhang Rong(Jon) <rong.zhang@windriver.com>
2021-11-05 11:28:34 +0800

    Add: flask api include resource type, resource pool, resource and deployment manager
    1. Create all metadata with mapper while execute start_o2ims_mappers()
    2. Extend domain class parameters and create subscription class
    3. Add flask API to get the list or specific one through the view level
    
    Change-Id: Id29b10172712c19cbfef57e6e74cd4bb85ad15eb
    Signed-off-by: Zhang Rong(Jon) <rong.zhang@windriver.com>

--- o2ims/entrypoints/flask_application.py
+++ o2ims/entrypoints/flask_application.py
@@ -21,13 +21,120 @@ from o2ims import bootstrap, config
 from o2ims.views import ocloud_view
 from o2ims.service.watcher.executor import start_watchers
 
+
+def configure_routes(app, bus):
+
+    # ----------  OClouds ---------- #
+    @app.route(apibase, methods=["GET"])
+    def oclouds():
+        result = ocloud_view.oclouds(bus.uow)
+        return jsonify(result), 200
+
+    # ----------  ResourceTypes ---------- #
+
+    @app.route(apibase + "/resourceTypes", methods=["GET"])
+    def resource_types():
+        result = ocloud_view.resource_types(bus.uow)
+        return jsonify(result), 200
+
+    @app.route(apibase + "/resourceTypes", methods=["POST", "PUT", "PATCH",
+                                                    "DELETE"])
+    def resource_types_not_allow():
+        return "Method Not Allowed", 405
+
+    @app.route(apibase + "/resourceTypes/<resourceTypeID>", methods=["GET"])
+    def resource_types_one(resourceTypeID):
+        result = ocloud_view.resource_type_one(resourceTypeID, bus.uow)
+        if result is None:
+            return "", 200
+        return jsonify(result), 200
+
+    @app.route(apibase + "/resourceTypes/<resourceTypeID>",
+               methods=["POST", "PUT", "PATCH", "DELETE"])
+    def resource_types_one_not_allow(resourceTypeID):
+        return "Method Not Allowed", 405
+
+    # ----------  ResourcePools ---------- #
+
+    @app.route(apibase + "/resourcePools", methods=["GET"])
+    def resource_pools():
+        result = ocloud_view.resource_pools(bus.uow)
+        return jsonify(result), 200
+
+    @app.route(apibase + "/resourcePools", methods=["POST", "PUT", "PATCH",
+                                                    "DELETE"])
+    def resource_pools_not_allow():
+        return "Method Not Allowed", 405
+
+    @app.route(apibase + "/resourcePools/<resourcePoolID>", methods=["GET"])
+    def resource_pools_one(resourcePoolID):
+        result = ocloud_view.resource_pool_one(resourcePoolID, bus.uow)
+        if result is None:
+            return "", 200
+        return jsonify(result), 200
+
+    @app.route(apibase + "/resourcePools/<resourcePoolID>",
+               methods=["POST", "PUT", "PATCH", "DELETE"])
+    def resource_pools_one_not_allow(resourcePoolID):
+        return "Method Not Allowed", 405
+
+    # ----------  Resources ---------- #
+
+    @app.route(apibase + "/resourcePools/<resourcePoolID>/resources",
+               methods=["GET"])
+    def resources(resourcePoolID):
+        result = ocloud_view.resources(resourcePoolID, bus.uow)
+        return jsonify(result), 200
+
+    @app.route(apibase + "/resourcePools/<resourcePoolID>/resources",
+               methods=["POST", "PUT", "PATCH", "DELETE"])
+    def resource_not_allow(resourcePoolID):
+        return "Method Not Allowed", 405
+
+    @app.route(apibase +
+               "/resourcePools/<resourcePoolID>/resources/<resourceID>",
+               methods=["GET"])
+    def resources_one(resourcePoolID, resourceID):
+        result = ocloud_view.resource_one(resourceID, bus.uow)
+        if result is None:
+            return "", 200
+        return jsonify(result), 200
+
+    @app.route(apibase +
+               "/resourcePools/<resourcePoolID>/resources/<resourceID>",
+               methods=["POST", "PUT", "PATCH", "DELETE"])
+    def resource_one_not_allow(resourcePoolID, resourceID):
+        return "Method Not Allowed", 405
+
+    # ----------  DeploymentManagers ---------- #
+
+    @app.route(apibase + "/deploymentManagers", methods=["GET"])
+    def deployment_managers():
+        result = ocloud_view.deployment_managers(bus.uow)
+        return jsonify(result), 200
+
+    @app.route(apibase + "/deploymentManagers",
+               methods=["POST", "PUT", "PATCH", "DELETE"])
+    def deployment_managers_not_allow():
+        return "Method Not Allowed", 405
+
+    @app.route(apibase + "/deploymentManagers/<deploymentManagerID>",
+               methods=["GET"])
+    def deployment_manager_one(deploymentManagerID):
+        result = ocloud_view.deployment_manager_one(
+            deploymentManagerID, bus.uow)
+        if result is None:
+            return "", 200
+        return jsonify(result), 200
+
+    @app.route(apibase + "/deploymentManagers/<deploymentManagerID>",
+               methods=["POST", "PUT", "PATCH", "DELETE"])
+    def deployment_manager_one_not_allow(deploymentManagerID):
+        return "Method Not Allowed", 405
+
+
 app = Flask(__name__)
 bus = bootstrap.bootstrap()
 apibase = config.get_o2ims_api_base()
 start_watchers()
-
-
-@app.route(apibase, methods=["GET"])
-def oclouds():
-    result = ocloud_view.oclouds(bus.uow)
-    return jsonify(result), 200
+configure_routes(app, bus)
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,17 @@ from pathlib import Path
 import pytest
 import redis
 import requests
+from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 from tenacity import retry, stop_after_delay
 
+from o2ims import config
 from o2ims.adapter.orm import metadata, start_o2ims_mappers
 from o2ims.adapter.clients.orm_stx import start_o2ims_stx_mappers
-from o2ims import config
-from o2ims.domain import stx_object as ocloudModel
+from o2ims.service import unit_of_work
+from o2ims.entrypoints import flask_application as flk_app
+from o2ims.bootstrap import bootstrap
 
 
 @pytest.fixture
@@ -28,6 +31,25 @@ def in_memory_sqlite_db():
 @pytest.fixture
 def sqlite_session_factory(in_memory_sqlite_db):
     yield sessionmaker(bind=in_memory_sqlite_db)
+
+
+@pytest.fixture
+def sqlite_uow(sqlite_session_factory):
+    uow = unit_of_work.SqlAlchemyUnitOfWork(
+        session_factory=sqlite_session_factory)
+    # with uow:
+    #     start_o2ims_mappers(uow.session.get_bind())
+    #     uow.commit()
+    yield uow
+    # clear_mappers()
+
+
+@pytest.fixture
+def sqlite_flask_uow(sqlite_uow):
+    app = Flask(__name__)
+    bus = bootstrap(False, sqlite_uow, )
+    flk_app.configure_routes(app, bus)
+    yield app.test_client()
 
 
 @pytest.fixture
@@ -56,7 +78,8 @@ def wait_for_redis_to_come_up():
 
 @pytest.fixture(scope="session")
 def postgres_db():
-    engine = create_engine(config.get_postgres_uri(), isolation_level="SERIALIZABLE")
+    engine = create_engine(config.get_postgres_uri(),
+                           isolation_level="SERIALIZABLE")
     wait_for_postgres_to_come_up(engine)
     metadata.create_all(engine)
     return engine
@@ -73,8 +96,16 @@ def postgres_session(postgres_session_factory):
 
 
 @pytest.fixture
+def postgres_uow(postgres_session_factory):
+    uow = unit_of_work.SqlAlchemyUnitOfWork(
+        session_factory=postgres_session_factory)
+    yield uow
+
+
+@pytest.fixture
 def restart_api():
-    (Path(__file__).parent / "../src/o2ims/entrypoints/flask_application.py").touch()
+    (Path(__file__).parent / "../src/o2ims/entrypoints/flask_application.py")\
+        .touch()
     time.sleep(0.5)
     wait_for_webapp_to_come_up()
 

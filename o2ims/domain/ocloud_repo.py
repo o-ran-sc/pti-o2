40fa7a3ddf21ccb563c823baffe7e70d2f1aebe2
Bin Yang <bin.yang@windriver.com>
2021-11-06 09:38:59 +0800

    Update watcher worker
    Issue-ID: INF-196
    Signed-off-by: Bin Yang <bin.yang@windriver.com>
    Change-Id: Ifcfea06b3f6263018e1d43d595f8b2f1545a54fe

---
+++ o2ims/domain/ocloud_repo.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2021 Wind River Systems, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import abc
+from typing import List, Set
+from o2ims.domain import ocloud
+
+
+class OcloudRepository(abc.ABC):
+    def __init__(self):
+        self.seen = set()  # type: Set[ocloud.Ocloud]
+
+    def add(self, ocloud: ocloud.Ocloud):
+        self._add(ocloud)
+        self.seen.add(ocloud)
+
+    def get(self, ocloudid) -> ocloud.Ocloud:
+        ocloud = self._get(ocloudid)
+        if ocloud:
+            self.seen.add(ocloud)
+        return ocloud
+
+    def list(self) -> List[ocloud.Ocloud]:
+        return self._list()
+
+    def update(self, ocloud: ocloud.Ocloud):
+        self._update(ocloud)
+
+    # def update_fields(self, ocloudid: str, updatefields: dict):
+    #     self._update(ocloudid, updatefields)
+
+    @abc.abstractmethod
+    def _add(self, ocloud: ocloud.Ocloud):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _get(self, ocloudid) -> ocloud.Ocloud:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _update(self, ocloud: ocloud.Ocloud):
+        raise NotImplementedError
--- tests/integration-ocloud/test_watcher_w_stx_sa.py
+++ tests/integration-ocloud/test_watcher_w_stx_sa.py
@@ -12,20 +12,19 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-# from datetime import datetime
-from flask import Flask, jsonify
-# request
-# from o2ims.domain import commands
-# from o2ims.service.handlers import InvalidResourceType
-from o2ims import bootstrap, config
-from o2ims.views import ocloud_view
-
-app = Flask(__name__)
-bus = bootstrap.bootstrap()
-apibase = config.get_o2ims_api_base()
+from multiprocessing.queues import Queue
+import pytest
+from o2ims.entrypoints.resource_watcher import start_watchers
+from multiprocessing import Process
+from multiprocessing import Pipe
+# pipe = Pipe()
+# q = Queue()
+import time
+# pytestmark = pytest.mark.usefixtures("mappers")
 
 
-@app.route(apibase, methods=["GET"])
-def oclouds():
-    result = ocloud_view.oclouds(bus.uow)
-    return jsonify(result), 200
+def test_watcher_service():
+    testedprocess = Process(target=start_watchers, args=())
+    testedprocess.start()
+    time.sleep(10)
+    testedprocess.terminate()

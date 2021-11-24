db24b63e41597cbc7171467517ccbb4eebefbf0d
Bin Yang <bin.yang@windriver.com>
2021-11-24 08:22:09 +0800

    Refactor o2dms api swagger
    Signed-off-by: Bin Yang <bin.yang@windriver.com>
    Change-Id: I996a66eb4f738c20b90e3fc75aa7dd7e2dadcf40

--- o2dms/views/dms_route.py
+++ o2dms/views/dms_route.py
@@ -17,79 +17,74 @@ from flask_restx import Resource
 
 from o2common.config import config
 from o2dms.views.dms_dto import DmsDTO, DmsLcmNfDeploymentDescriptorDTO
-from o2dms.views import dms_lcm_view
+from o2dms.views import dms_lcm_view, api_dms_lcm_v1
 
 apibase = config.get_o2dms_api_base()
 
 
 # ----------  DeploymentManagers ---------- #
-api_dms = DmsDTO.api
-
-
-@api_dms.route("/<deploymentManagerID>")
-@api_dms.param('deploymentManagerID', 'ID of the deployment manager')
-@api_dms.response(404, 'Deployment manager not found')
+@api_dms_lcm_v1.route("/<deploymentManagerID>")
+@api_dms_lcm_v1.param('deploymentManagerID', 'ID of the deployment manager')
+@api_dms_lcm_v1.response(404, 'Deployment manager not found')
 class DmsGetRouter(Resource):
 
     model = DmsDTO.dms_get
 
-    @api_dms.doc('Get deployment manager')
-    @api_dms.marshal_with(model)
+    @api_dms_lcm_v1.doc('Get deployment manager')
+    @api_dms_lcm_v1.marshal_with(model)
     def get(self, deploymentManagerID):
         result = dms_lcm_view.deployment_manager_one(
             deploymentManagerID, uow)
         if result is not None:
             return result
-        api_dms.abort(404, "Deployment manager {} doesn't exist".format(
+        api_dms_lcm_v1.abort(404, "Deployment manager {} doesn't exist".format(
             deploymentManagerID))
 
 
 # LCM services #
-api_lcm_nfdeploymentDesc = DmsLcmNfDeploymentDescriptorDTO.api
-
-
-@api_lcm_nfdeploymentDesc\
-    .route("/<deploymentManagerID>/O2dms_DeploymentLifecycle")
-@api_lcm_nfdeploymentDesc\
+@api_dms_lcm_v1\
+    .route("/<deploymentManagerID>/O2dms_DeploymentLifecycle/"
+           "NfDeploymentDescriptor")
+@api_dms_lcm_v1\
     .param('deploymentManagerID', 'ID of the deployment manager')
-@api_lcm_nfdeploymentDesc.response(404, 'DMS LCM not found')
+@api_dms_lcm_v1.response(404, 'DMS LCM not found')
 class DmsLcmNfDeploymentDescListRouter(Resource):
 
     model = DmsLcmNfDeploymentDescriptorDTO.dmslcm_NfDeploymentDescriptor_get
 
-    @api_lcm_nfdeploymentDesc.doc('Get a list of NfDeploymentDescriptor')
-    @api_lcm_nfdeploymentDesc.marshal_list_with(model)
+    @api_dms_lcm_v1.doc('Get a list of NfDeploymentDescriptor')
+    @api_dms_lcm_v1.marshal_list_with(model)
     def get(self, deploymentManagerID):
         return dms_lcm_view.lcm_nfdeploymentdesc_list(deploymentManagerID, uow)
 
 
-@api_lcm_nfdeploymentDesc\
+@api_dms_lcm_v1\
     .route("/<deploymentManagerID>/O2dms_DeploymentLifecycle/"
-           "<nfDeploymentDescriptorId>")
-@api_lcm_nfdeploymentDesc\
+           "NfDeploymentDescriptor/<nfDeploymentDescriptorId>")
+@api_dms_lcm_v1\
     .param('deploymentManagerID', 'ID of the deployment manager')
-@api_lcm_nfdeploymentDesc.param('nfDeploymentDescriptorId',
-                                'ID of the NfDeploymentDescriptor')
-@api_lcm_nfdeploymentDesc.response(404, 'DMS LCM not found')
+@api_dms_lcm_v1.param('nfDeploymentDescriptorId',
+                      'ID of the NfDeploymentDescriptor')
+@api_dms_lcm_v1.response(404, 'DMS LCM not found')
 class DmsLcmNfDeploymentDescGetRouter(Resource):
 
     model = DmsLcmNfDeploymentDescriptorDTO.dmslcm_NfDeploymentDescriptor_get
 
-    @api_lcm_nfdeploymentDesc.doc('Get a NfDeploymentDescriptor')
-    @api_lcm_nfdeploymentDesc.marshal_with(model)
+    @api_dms_lcm_v1.doc('Get a NfDeploymentDescriptor')
+    @api_dms_lcm_v1.marshal_with(model)
     def get(self, nfDeploymentDescriptorId, deploymentManagerID):
         result = dms_lcm_view\
             .lcm_nfdeploymentdesc_one(nfDeploymentDescriptorId,
                                       deploymentManagerID, uow)
         if result is not None:
             return result
-        api_dms.abort(404, "NfDeploymentDescriptor {} doesn't exist".format(
-            nfDeploymentDescriptorId))
+        api_dms_lcm_v1.abort(
+            404, "NfDeploymentDescriptor {} doesn't exist".format(
+                nfDeploymentDescriptorId))
 
 
 def configure_namespace(app, bus):
-    app.add_namespace(api_dms, path=apibase)
-    app.add_namespace(api_lcm_nfdeploymentDesc, path=apibase)
+    app.add_namespace(api_dms_lcm_v1, path=apibase)
 
     # Set global uow
     global uow

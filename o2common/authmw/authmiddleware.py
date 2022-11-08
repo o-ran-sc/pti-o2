c7ce3d4bcd4e86d863d271ad583d69f048377cf8
dliu5 <david.liu@windriver.com>
2022-11-08 13:41:38 +0800

    Improve the ssl connection handle log message. Fix auth header exception handling to avoid 500.
    Signed-off-by: dliu5 <david.liu@windriver.com>
    Change-Id: I7fb0d11584f6e575ba891a51bebd7631faef7505

--- o2common/authmw/authmiddleware.py
+++ o2common/authmw/authmiddleware.py
@@ -61,8 +61,10 @@ class authmiddleware():
         logger.info(__name__ + 'authentication middleware')
         req = Request(environ, populate_request=True, shallow=True)
         try:
-            auth_header = req.headers['Authorization']
-
+            try:
+                auth_header = req.headers['Authorization']
+            except Exception as ex:
+                raise AuthRequiredExp('Bearer realm="Authentication Required"')
             if auth_header:
                 auth_token = auth_header.split(" ")[1]
 

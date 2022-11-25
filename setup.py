f21e16f8ef8fb20806195752612960bffce8120b
Bin Yang <bin.yang@windriver.com>
2022-11-25 21:07:57 +0800

    Update package description
    Issue-ID: INF-384
    
    Signed-off-by: Bin Yang <bin.yang@windriver.com>
    Change-Id: Ied0326921803be7d1a8b6bb576b1e4f2a8662638

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from setuptools import find_packages
 
 setup(
     name="o2imsdms",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
-    license="LICENSE",
-    description="Represent O2 IMS and O2 DMS",
+    license="Apache-2.0",
+    author='Bin Yang',
+    author_email='bin.yang@windriver.com',
+    description="Represent StarlingX as an O-RAN O-Cloud by exposing O-RAN Compliant O2 IMS and O2 DMS Interfaces",
+    url='https://docs.o-ran-sc.org/projects/o-ran-sc-pti-o2/en/latest/index.html',
     install_requires=[
         'httplib2',
-        # 'distributedcloud-client',
-        # 'cgtsclient',
+        'distributedcloud-client',
+        'cgtsclient',
+        'fmclient',
         'babel',  # Required by distributedcloud-client
         'PrettyTable<0.8,>=0.7.2',  # Required by distributedcloud-client
     ]

666bfb43a7a444e48ce03dbf0c96b4d40deb9a29
Zhang Rong(Jon) <rong.zhang@windriver.com>
2024-04-25 23:12:19 +0800

    Add the PATCH method for the monitoring API
    Since the specification release, a new API has been added to the
    monitoring interface. Which is an acknowledge/clear operation for
    the alarm event.
    
    This commit will support SMO requests the O2IMS API to
    acknowledge or clear the alarm event.
    The acknowledge will update the alarmEventRecord object, mark
    the acknowledged as True.
    The clear operation will async call the fault client to delete
    the alarm of the O-Cloud.
    
    Test Plan:
    1. Request the API to acknowledge/clear the alarm.
    2. Only supported one request from one of the acknowledge or
       clear.
    3. The clear request should with "5" that is "CLEARED".
    4. After request the alarm clear, it was requested the O-Cloud
       to delete its alarm.
    
    Issue-ID: INF-449
    
    Change-Id: I71419a9d467687d6708bfd3e9cd201dbce5e36c5
    Signed-off-by: Zhang Rong(Jon) <rong.zhang@windriver.com>

--- o2app/entrypoints/redis_eventconsumer.py
+++ o2app/entrypoints/redis_eventconsumer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Wind River Systems, Inc.
+# Copyright (C) 2021-2024 Wind River Systems, Inc.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -49,6 +49,7 @@ def main():
     pubsub.subscribe('DmsChanged')
     pubsub.subscribe('ResourceChanged')
     pubsub.subscribe('AlarmEventChanged')
+    pubsub.subscribe('AlarmEventPurged')
 
     for m in pubsub.listen():
         try:
@@ -136,6 +137,14 @@ def handle_changed(m, bus):
         cmd = imscmd.PubAlarm2SMO(data=AlarmEvent2SMO(
             id=data['id'], ref=ref,
             eventtype=data['notificationEventType'],
+            updatetime=data['updatetime']))
+        bus.handle(cmd)
+    elif channel == 'AlarmEventPurged':
+        datastr = m['data']
+        data = json.loads(datastr)
+        logger.info('AlarmEventPurged with cmd:{}'.format(data))
+        cmd = imscmd.PurgeAlarmEvent(data=AlarmEvent2SMO(
+            id=data['id'], eventtype=data['notificationEventType'],
             updatetime=data['updatetime']))
         bus.handle(cmd)
     else:
